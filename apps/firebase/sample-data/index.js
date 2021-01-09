03121fbf8dbd0188e684fffa77df8ea2cf2808bb
Jerry Orta <jerryorta.dev@gmail.com>
2021-01-09 14:33:29 -0600

    feat(firebase): stub data

--- apps/firebase/sample-data/index.js
+++ apps/firebase/sample-data/index.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 const admin = require('firebase-admin');
 
 /**
@@ -6,4 +6,26 @@ const admin = require('firebase-admin');
  */
 const serviceAccount = require('../../../../torqata-service-keys/torqata-movies-firebase-adminsdk-bcwf7-671c36cc8c.json');
 
+const firestorePath = 'movies/netflix/titles';
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://spout-dev.firebaseio.com',
+});
+
+const devDb = admin.firestore();
+
+let data = fs.readFileSync('./data/netflix_titles.json', 'utf8');
+data = JSON.parse(data);
+
+data.forEach((movieTitle) => {
+  if (movieTitle) {
+    devDb
+      .collection(firestorePath)
+      .doc(movieTitle.show_id)
+      .set(movieTitle)
+      .then(() => {
+        console.log('movie tile: ', movieTitle.title, 'written');
+      });
+  }
+});
--- docs/firebase.md
+++ docs/firebase.md
@@ -30,10 +30,12 @@ To test, create a PR and you should see a github action in the PR set up a revie
 
 Use working directory `./apps/firebase/sample-data`;
 
+- [ ] install firebase-import by running `npm install -g firebase-import`
 - [ ] Download data from [here](https://www.kaggle.com/shivamb/netflix-shows)
 - [ ] Set up [firebase service account key](https://console.firebase.google.com/project/torqata-movies/settings/serviceaccounts/adminsdk)
 - [ ] Download service account json file external to this repo.
-- [ ] Convert CSV data to json using [node-csvtojson](https://github.com/Keyang/node-csvtojson)
+- [ ] Convert CSV data to json using [node-csvtojson](https://github.com/Keyang/node-csvtojson) by running `node node csv-to-json.js`.
+- [ ] Import to firestore by running 
 
 
 
