44899774135a7131e9b540fcf718461ba2f5eb5e
Jerry Orta <jerryorta.dev@gmail.com>
2021-01-10 05:34:09 -0600

    feat(movies): wip

--- apps/movies/src/app/app.module.ts
+++ apps/movies/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
+
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
