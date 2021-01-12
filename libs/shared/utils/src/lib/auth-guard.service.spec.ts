850c42263f56b7ec73d2ece2d35b57a85e6a1783
Jerry Orta <jerryorta.dev@gmail.com>
2021-01-12 11:57:33 -0600

    test(shared-utils): tests pass

--- libs/shared/utils/src/lib/auth-guard.service.spec.ts
+++ libs/shared/utils/src/lib/auth-guard.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { initialAccountState } from '@tor/shared/models';
 
 import { AuthGuardService } from './auth-guard.service';
 
 describe('AuthGuardService', () => {
   let service: AuthGuardService;
 
+  let store: MockStore;
+  const initialState = {
+    ...initialAccountState,
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })],
+    });
     service = TestBed.inject(AuthGuardService);
   });
 
